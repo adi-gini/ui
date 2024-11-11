9151698b2f37e6be2a516c70196b72959acc3445
adi-gini <adiu@gini-apps.com>
2024-11-11 10:44:53 +0200

    add: DemoData file

--- src/components/DetailsAlerts/DetailsAlerts.js
+++ src/components/DetailsAlerts/DetailsAlerts.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import demoDataAlerts from '../../utils/demoDataAlerts'
+import { useSelector } from 'react-redux'
+import DatePicker from '../../common/DatePicker/DatePicker'
+import { TIME_FRAME_LIMITS } from '../../utils/datePicker.util'
 
-const DetailsAlerts = ({ selectedItem }) => {
+// import MetricsSelector from '../../elements/MetricsSelector/MetricsSelector'
+// import modelEndpointsActions from '../../actions/modelEndpoints'
+// import DatePicker from '../../common/DatePicker/DatePicker'
+// import { TIME_FRAME_LIMITS } from '../../utils/datePicker.util'
+
+const DetailsAlerts = () => {
+  const detailsStore = useSelector(store => store.detailsStore)
+
   return (
     <div>
-      <h2>Alerts</h2>
-      <p>This tab shows alerts for the selected item.</p>
-      {/* Add more content here as needed */}
+      <div className="metrics__custom-filters"></div>
+      <DatePicker
+        className="details-date-picker"
+        date={detailsStore.dates.value[0]}
+        dateTo={detailsStore.dates.value[1]}
+        selectedOptionId={detailsStore.dates.selectedOptionId}
+        label=""
+        onChange={() => {}}
+        type="date-range-time"
+        timeFrameLimit={TIME_FRAME_LIMITS.MONTH}
+        withLabels
+      />
+      <ul>
+        {demoDataAlerts.map(alert => (
+          <li key={alert.name}>
+            <h3>{alert.name}</h3>
+            <p>Project: {alert.project}</p>
+            <p>Severity: {alert.severity}</p>
+            <p>Event Kind: {alert.event_kind}</p>
+            <p>Number of Events: {alert.number_of_events}</p>
+            <p>Activation Time: {new Date(alert.activation_time).toLocaleString()}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
